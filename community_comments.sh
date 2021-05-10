057576c8afe77859ae0bfcfd6d09fc809d79dcdd
Janne Spijkervet <janne.spijkervet@gmail.com>
2021-05-10 09:23:56 +0200

    added sh script

---
+++ community_comments.sh
@@ -0,0 +1,1 @@
+python -m community_page.main $@
--- community_page/main.py
+++ community_page/main.py
@@ -33,27 +33,31 @@ def scroll_driver(scroll_pause_time, max_scrolls):
 
 
 if __name__ == "__main__":
-    options = Options()
-    options.headless = True
-    driver = webdriver.Chrome(chrome_options=options)
-
     if len(sys.argv) < 2:
         raise Exception(
             "Not enough arguments, pass the community poll URL after the command."
         )
+    
+    options = Options()
+    options.headless = True
+    driver = webdriver.Chrome(options=options)
+
 
     url = sys.argv[1]
     driver.get(url)
 
     # click privacy agreement button
+    print("Scrolling page...")
     button = driver.find_element_by_xpath('//button[normalize-space()="I agree"]')
     button.click()
     time.sleep(3)
 
     # scroll down page
     scroll_driver(scroll_pause_time=0.1, max_scrolls=300)
+    print("Scraping comments...")
     comments = get_community_page_comments()
 
+    print("Calculating named entities...")
     entities = Counter()
     nlp = spacy.load("en_core_web_md")
     for c in comments:
@@ -63,7 +67,8 @@ if __name__ == "__main__":
             x_processed = x.text.lower()
             entities[x_processed] += 1
     driver.quit()
-
+    
+    print("Saving .csv files...")
     df = pd.DataFrame(comments, columns=["comment"])
     df.to_csv("community_poll_comments.csv", index=False)
     write_frequent_terms(entities, "community_poll_terms.csv")
